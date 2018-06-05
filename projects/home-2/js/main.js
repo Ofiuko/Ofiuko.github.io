19561bf30797a5178172656e1d7c024554b38733
Elena Schleht <39513776+ofiuko@users.noreply.github.com>
2018-06-06 00:18:34 +0300

    Update main.js

--- projects/home-2/js/main.js
+++ projects/home-2/js/main.js
@@ -1,6 +1,6 @@
 $(function () {
     //Toggle menu
     $('.mobile').click(function() {
-        $('.menu').toggleClass('active');
+        $('.menu, .header__menu, .mobile').toggleClass('active');
     });
 });
